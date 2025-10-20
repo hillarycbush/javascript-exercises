c246d45a4e21b64e66fec4be09fbd31d1c6e838f
hillarycbush <hillarycbush@gmail.com>
2025-10-19 20:41:29 -0700

    passed test

--- 02_addNumbers/addNumbers.js
+++ 02_addNumbers/addNumbers.js
@@ -1,13 +1,15 @@
+<p>passed</p>
+
 function addNumbers() {
   const a = 1;
   const b = 1;
 
   let result;
 
-  result = "a" + "b"; // <------ EDIT THIS LINE
-
+  result = a + b; // <------ EDIT THIS LINE
   return result;
+  
 }
-
+console.log(addNumbers());
 // Do not change this
 module.exports = addNumbers;
